b6962fd3b6722b3d9d0b6bb7c3924821451947cd
PedroBSantos <pedrobsantos98@outlook.com>
2021-01-04 15:28:02 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/DSDeliverApplication.java
@@ -0,0 +1,12 @@
+package com.devsuperior.dsdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DSDeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DSDeliverApplication.class, args);
+	}
+}
